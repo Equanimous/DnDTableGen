fa44a003eb33a85aab63ec852955426d0356c6d2
U-Matt-PC\Matt <mattcperl@gmail.com>
2018-06-09 16:18:51 -0400

    Added filePicker impl, fileReading impl, and randomizing data in list impl

--- FileIO.cs
+++ FileIO.cs
@@ -12,8 +12,33 @@ namespace DnDTableGen
         public List<string> filePicker()
         {
             List<string> fileList = null;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.InitialDirectory = "C:\\";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileList = new List<string>(openFileDialog.FileNames);
+            }
 
             return fileList;
+        }
+
+        public List<string> readFromFiles(List<string> fileList)
+        {
+            List<string> dataList = new List<string>();
+
+            foreach (string pathName in fileList)
+            {
+                string[] fileData = System.IO.File.ReadAllLines(pathName);
+
+                for (int i = 0; i < fileData.Length; i++)
+                {
+                    dataList.Add(fileData[i]);
+                }
+            }
+
+            return dataList;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,18 +8,33 @@ namespace DnDTableGen
 {
     public class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
             FileIO fileIO = new FileIO();
+            List<string> fileList = new List<string>();
             List<string> dataList = new List<string>();
+            Random random = new Random();
 
             //pick folder to fetch files from
             //store file names in array or list
-            dataList = fileIO.filePicker();
+            fileList = fileIO.filePicker();
 
-            //read \n-separated data to data list (do not allow duplicates)
-            //randomize data in list
+            //read \n delimited data to data list
+            dataList = fileIO.readFromFiles(fileList);
+
+            //randomize data in list (simple solution, can be built on)
+            for (int i = 0; i < dataList.Count; i++)
+            {
+                string tmpData = dataList[i];
+                int randInd = random.Next(i, dataList.Count);
+                dataList[i] = dataList[randInd];
+                dataList[randInd] = tmpData;
+            }
+
             //based on number of data items, determine how to assign probability to each object -- could use strategy pattern if program is extended
+
+
             //write data out to file in the form of {probability} {\t} {data object} {\n}
         }
     }
